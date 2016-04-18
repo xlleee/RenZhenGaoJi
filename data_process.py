5e920e2be851c69c6571a245427d855249f53ae4
xlleee <xlleee@gmail.com>
2016-04-18 20:36:03 +0800

    20160418
    20160418

--- data_process.py
+++ data_process.py
@@ -34,6 +34,25 @@ sql_allmng = """
     """
 data_allmng = pd.read_sql(sql_allmng, cnxn_jrgcb)
 ########################################################
+# read index data for JYDB
+sql_betaindex = """
+    SELECT A.SecuCode, B.TradingDay, B.ChangePCT, A.ChiNameAbbr, A.ChiName
+    FROM [JYDB].[dbo].[SecuMain] A, [JYDB].[dbo].[QT_IndexQuote] B
+    WHERE A.InnerCode = B.InnerCode AND A.SecuCode in ('000300','000905','000852')
+    ORDER BY A.SecuCode, B.TradingDay"""
+data_betaindex = pd.read_sql(sql_betaindex, cnxn_jydb)
+sql_induindex = """
+    SELECT A.SecuCode, B.TradingDay, B.ChangePCT, A.ChiNameAbbr, A.ChiName, A.InnerCode
+    FROM [JYDB].[dbo].[SecuMain] A, [JYDB].[dbo].[QT_IndexQuote] B
+    WHERE A.InnerCode = B.InnerCode AND A.SecuCode in
+    ('CI005001','CI005002','CI005003','CI005004','CI005005',
+    'CI005006','CI005007','CI005008','CI005009','CI005010',
+    'CI005011','CI005012','CI005013','CI005014','CI005015',
+    'CI005016','CI005017','CI005018','CI005019','CI005020',
+    'CI005021','CI005022','CI005023','CI005024','CI005025',
+    'CI005026','CI005027','CI005028','CI005029')
+    ORDER BY A.SecuCode, B.TradingDay"""
+data_induindex = pd.read_sql(sql_induindex, cnxn_jydb)
 
 
 
@@ -41,13 +60,12 @@ data_allmng = pd.read_sql(sql_allmng, cnxn_jrgcb)
 
 
 
-
-
-def organize_data(ManagerID, cnxn_jydb, cnxn_jrgcb):
+def organize_data(ManagerID, cnxn_jydb, cnxn_jrgcb, data_betaindex, data_induindex):
+    # read mng record data
     sql_allrecord = """
         SELECT [InnerCode], [EndDate], [dailyreturn], [FundsofManager], [ManagersofFund]
         FROM [jrgcb].[dbo].[FundAndManagerData]
         WHERE [ManagerID] = """ + ManagerID + """
         ORDER BY [EndDate]
         """
-    
+    data_allrecord = read_sql(sql_allrecord, cnxn_jrgcb)
